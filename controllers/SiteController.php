d6bfde0588eed92326c5fb9bef7a02e21c348299
123rofiq <rofiq.hasansah@gmail.com>
2018-02-12 20:11:47 +0700

    baru

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -69,20 +69,21 @@ class SiteController extends Controller
      *
      * @return Response|string
      */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
+     public function actionLogin()
+       {
+           if (!\Yii::$app->user->isGuest) {
+               return $this->goHome();
+           }
 
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        }
-        return $this->render('login', [
-            'model' => $model,
-        ]);
-    }
+           $model = new LoginForm();
+           if ($model->load(Yii::$app->request->post()) && $model->login()) {
+               return $this->goBack();
+           } else {
+               return $this->render('login', [
+                   'model' => $model,
+               ]);
+           }
+       }
 
     /**
      * Logout action.
